40154206911f762035d581a6b75383e1eb3135c8
HAGG-glitch <joshuayoki1@gmail.com>
2026-06-21 21:28:32 +0000

    Update Moses Moore subscriberId to Monime UUID

--- migrations/000006_ussd_allowed_subscribers.up.sql
+++ migrations/000006_ussd_allowed_subscribers.up.sql
@@ -15,7 +15,7 @@ ON ussd_allowed_subscribers (subscriber_id_hash, is_active);
 INSERT INTO ussd_allowed_subscribers (subscriber_id_hash, label, is_active)
 VALUES
 ('3d051666-0faf-57ee-acd7-3850e5f46501', 'Nelson', TRUE),
-('82a05af957af142cd35af548d76596ba0cf21730023e032adaef2c7c279d7270', 'Moses Moore', TRUE),
+('82769d25-da42-5389-b795-35fb7d9c4410', 'Moses Moore', TRUE),
 ('b7e8504f4419ded90df8d15d2846814894b1fdfb0ad84f4974709ee0d94c3e87', 'Mr Cyril', TRUE),
 ('bdb7b76c58a77bd2dc791a01e19651276e820b4e0ad8f9568482dde3b0e0579d', 'Sanah', TRUE),
 ('f6a5d604c84acfb1a0904456d7b753407c4b7aad81f2bd18e5f2df6a885d5b95', 'Joshua Yoki', TRUE),
